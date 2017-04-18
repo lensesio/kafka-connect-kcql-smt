664c459d8c87c9aaf4ba5d0dd3cc4149a6a47a03
stefan bocutiu <stefan.bocutiu@gmail.com>
2017-04-18 22:01:29 +0100

    upgrading to json-sql 2.0

--- src/main/scala/com/landoop/connect/sql/Transform.scala
+++ src/main/scala/com/landoop/connect/sql/Transform.scala
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.landoop.connect.sql.StructSql._
 import com.landoop.json.sql.JacksonJson
 import com.landoop.json.sql.JsonSql._
-import com.typesafe.scalalogging.StrictLogging
+import com.typesafe.scalalogging.slf4j.StrictLogging
 import org.apache.calcite.sql.SqlDialect
 import org.apache.kafka.connect.data.{Schema, Struct}
 
